e64929c5543dc2a019a57416c0bfbd1949b70a88
Swiftb0y <Swiftb0y@users.noreply.github.com>
2025-08-30 09:50:57 +0000

    deploy: 3dcc01742e3813ce2cab4c59cd4c5af8656a63b2

--- source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"rekordcrate":["",[["pdb",[],["mod.rs","string.rs"]]],["anlz.rs","lib.rs","setting.rs","util.rs","xor.rs"]]\
-}');
-createSourceSidebar();
---
+++ src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["rekordcrate",["",[["pdb",[],["mod.rs","string.rs"]]],["anlz.rs","lib.rs","setting.rs","util.rs","xml.rs","xor.rs"]]]]');
+//{"start":19,"fragment_lengths":[118]}
